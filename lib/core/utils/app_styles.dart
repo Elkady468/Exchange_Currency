8086620a249540ab719fc8f7bdfad4d5e5579d1a
abdallahelkady <abdallahelkady06@gmail.com>
2025-08-22 23:54:40 +0300

    add app styles

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/widgets.dart';
+
+class Styles {
+  static TextStyle Mediume20(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
+      fontWeight: FontWeight.w500,
+      color: Color(0xff1F2261),
+    );
+  }
+
+  static TextStyle Reguler14(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontWeight: FontWeight.w400,
+      color: Color(0xff808080),
+    );
+  }
+
+  static TextStyle Reguler12(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
+      fontWeight: FontWeight.w400,
+      color: Color(0xffA6A1A1),
+    );
+  }
+
+  static TextStyle Reguler16(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w400,
+      color: Color(0xff808080),
+    );
+  }
+
+  static TextStyle Mediume18(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
+      fontWeight: FontWeight.w500,
+      color: Color(0xff3C3C3C),
+    );
+  }
+
+  static TextStyle Mediume16(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w500,
+      color: Color(0xff26278D),
+    );
+  }
+}
+
+double getResponsiveFontSize(context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+
+  double lowerLimit = fontSize * .8;
+  double upperLimit = fontSize * 1.2;
+
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(context) {
+  // var dispatcher = PlatformDispatcher.instance;
+  // var physicalWidth = dispatcher.views.first.physicalSize.width;
+  // var devicePixelRatio = dispatcher.views.first.devicePixelRatio;
+  // double width = physicalWidth / devicePixelRatio;
+
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < 800) {
+    return width / 375;
+  } else if (width < 1200) {
+    return width / 1000;
+  } else {
+    return width / 1440;
+  }
+}
