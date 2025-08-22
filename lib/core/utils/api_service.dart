eb9c28c449ad2358547d64e6b4fe0322780025a2
abdallahelkady <abdallahelkady06@gmail.com>
2025-08-22 22:10:07 +0300

    add api service

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,1 +1,16 @@
+import 'package:dio/dio.dart';
 
+class ApiService {
+  ApiService(this._dio);
+
+  final Dio _dio;
+
+  final String basrUrl =
+      "https://v6.exchangerate-api.com/v6/b923f696585753515bbd2dcc/latest/";
+
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var response = await _dio.get("$basrUrl$endPoint");
+
+    return response.data;
+  }
+}
